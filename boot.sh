ea801136da965c40361f5c982c66b781e3c29e27
Delocalized Bond <editor@delocalized.bond>
2025-06-24 11:02:38 +0400

    updates and upgrades can only be done by a sudoer

--- boot.sh
+++ boot.sh
@@ -12,8 +12,7 @@ echo -e "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04+ or Debian 12+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo apt-get update >/dev/null
-sudo apt-get install -y git >/dev/null
+
 
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
