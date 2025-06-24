4a054cecee3dcb0e4206050248d0cb459f65292b
Delocalized Bond <editor@delocalized.bond>
2025-06-24 17:05:20 +0400

    chore: remove obsolete app install scripts from repo

--- install/desktop/app-libreoffice.sh
+++
@@ -1,7 +0,0 @@
-# Install LibreOffice
-if [ "$OMAKUB_OS_ID" = "ubuntu" ] || [ "$OMAKUB_OS_ID" = "debian" ]; then
-  sudo apt install -y libreoffice
-else
-  echo "Unsupported OS for LibreOffice installation."
-  exit 1
-fi
