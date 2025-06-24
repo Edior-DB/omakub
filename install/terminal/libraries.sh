c056d0b732363bf78b6d4af0224680af4bdb1b9b
Delocalized Bond <editor@delocalized.bond>
2025-06-24 20:26:11 +0400

    Remove shebang from libraries.sh since it is always sourced, not executed directly. Clean up for correct sourcing behavior.

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,12 +1,22 @@
+# This script installs core libraries required for various applications and tools on Ubuntu or Debian systems.
+
+apt='apt'
+
+if command -v gum >/dev/null 2>&1; then
+  if gum confirm "Do you want to install nala? It works faster..."; then
+    sudo apt install -y nala && apt='nala'
+  fi
+fi
+
 # Install core libraries (including MySQL/MariaDB dev)
 if [ "$OMAKUB_OS_ID" = "ubuntu" ]; then
-  sudo apt install -y \
+  sudo "$apt" install -y \
     build-essential pkg-config autoconf bison clang rustc \
     libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
     libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
     redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
 elif [ "$OMAKUB_OS_ID" = "debian" ]; then
-  sudo apt install -y \
+  sudo "$apt" install -y \
     build-essential pkg-config autoconf bison clang rustc \
     libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
     libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
