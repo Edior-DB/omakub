f15ab0ed1634fcb34ce671b6619015fa02046a34
Delocalized Bond <editor@delocalized.bond>
2025-06-25 13:32:34 +0400

    fix: source check-version.sh and use Debian-compatible awk for installer selection

--- bin/omakub-sub/install.sh
+++ bin/omakub-sub/install.sh
@@ -44,7 +44,11 @@ elif [[ "$CHOICE" == "> All"* ]]; then
     gum spin --spinner globe --title "Install completed!" -- sleep 3
   fi
 else
-  INSTALLER=$(echo "$CHOICE" | awk -F ' {2,}' '{print $1}' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+  if [ "$OMAKUB_OS_ID" = "debian" ]; then
+    INSTALLER=$(echo "$CHOICE" | awk '{print $1}' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+  else
+    INSTALLER=$(echo "$CHOICE" | awk -F ' {2,}' '{print $1}' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+  fi
   case "$INSTALLER" in
     "dev-editor") INSTALLER_FILE="$OMAKUB_PATH/bin/omakub-sub/install-dev-editor.sh" ;;
     "web-apps") INSTALLER_FILE="$OMAKUB_PATH/install/desktop/optional/select-web-apps.sh" ;;
