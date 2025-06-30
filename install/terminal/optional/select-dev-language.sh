10230fbece9243592dac36748d113a6616395f7c
Delocalized Bond <editor@delocalized.bond>
2025-06-30 14:12:14 +0400

    Install PHP 8.2 and modules on Debian 12, PHP 8.4 from PPA on Ubuntu

--- install/terminal/optional/select-dev-language.sh
+++ install/terminal/optional/select-dev-language.sh
@@ -21,8 +21,12 @@ if [[ -n "$languages" ]]; then
       mise use --global go@latest
       ;;
     PHP)
-      sudo add-apt-repository -y ppa:ondrej/php
-      sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+      if [ "$OMAKUB_OS_ID" = "ubuntu" ]; then
+        sudo add-apt-repository -y ppa:ondrej/php
+        sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+      elif [ "$OMAKUB_OS_ID" = "debian" ]; then
+        sudo apt -y install php8.2 php8.2-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+      fi
       php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
       php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
       rm composer-setup.php
